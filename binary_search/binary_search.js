f52ea7fc8ce671d21829d46ce067202a8b471cfe
metherton <martinetherton63@gmail.com>
2016-10-12 21:41:32 +0200

    added binary search

--- binary_search/binary_search.js
+++ binary_search/binary_search.js
@@ -1,6 +1,6 @@
 var readline = require('readline');
 var lineNumber = 0;
-var firstLine, secondLine;
+var listOfNumbers, keysToSearch;
 
 process.stdin.setEncoding('utf8');
 
@@ -14,11 +14,11 @@ rl.on('line', readLine);
 function readLine(line) {
     if (line !== "\n") {
         if (lineNumber === 0) {
-            firstLine = line.toString().split(' ');
+            listOfNumbers = line.toString().split(' ');
             lineNumber++;
         } else {
-            secondLine = line.toString().split(' ');
-            console.log(BINARY_SEARCH.calculate(firstLine.map(strToInt), secondLine.map(strToInt)));
+            keysToSearch = line.toString().split(' ');
+            console.log(BINARY_SEARCH.calculate(listOfNumbers.map(strToInt), keysToSearch.map(strToInt)));
             process.exit();
         }
     }
@@ -30,8 +30,22 @@ function strToInt(item) {
 
 var BINARY_SEARCH = {} || BINARY_SEARCH;
 
-BINARY_SEARCH.calculate = function(firstLine, secondLine) {
-    console.log('first',firstLine);
-    console.log('secibd',secondLine);
-    return 0;
+BINARY_SEARCH.calculate = function(listOfNumbers, keysToSearch) {
+    function bs(nums, low, high, search) {
+        var mid;
+        if (high < low) {
+            return low - 1;
+        }
+        mid = low + Math.floor((high - low) / 2);
+        if (search === nums[mid]) {
+            return mid;
+        } else if (search < nums[mid]) {
+            return bs(nums, low, mid - 1, search);
+        } else {
+            return bs(nums, mid + 1, high, search);
+        }
+    }
+    var numberOfItems = listOfNumbers.slice(0,1);
+    var items = listOfNumbers.slice(1,listOfNumbers.length);
+    return bs(items, 0, items.length - 1, 3);
 };
